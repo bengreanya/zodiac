89536f4c9fc1580c652c6f8199a4350d4b512bf4
Ben Greanya <bengreanya@gmail.com>
2023-01-09 18:06:37 -0800

    background sort of working

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+import background from '../../background.jpeg';
+// import ZodiacCard from '../ZodiacCard/Zodiac.js';
+// import { zodiac } from '../../data.js';
+
+// import React from 'react';
+
+export default function Main() {
+  return <div style={{ backgroundImage: `url(${background})` }}>something</div>;
+}
