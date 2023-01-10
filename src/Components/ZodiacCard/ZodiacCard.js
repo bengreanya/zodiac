39d1ea5ba5717d9c4fa46175b789b08288b437e7
Ben Greanya <bengreanya@gmail.com>
2023-01-10 15:10:17 -0800

    got images to display

--- src/Components/ZodiacCard/ZodiacCard.js
+++ src/Components/ZodiacCard/ZodiacCard.js
@@ -5,7 +5,7 @@ export default function ZodiacCard(props) {
   return (
     <div className="zodiacCard">
       ZodiacCard
-      <img src={`images/${props.image}.png`} />
+      <img src={`images/${props.name}.png`} />
       <h2>{props.name}</h2>
       <p>{props.dates}</p>
     </div>
