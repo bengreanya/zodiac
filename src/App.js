3c13559f1647aac09e7a6cc9a6b13daebfaabc2b
Ben Greanya <bengreanya@gmail.com>
2023-01-10 15:31:17 -0800

    now theres a footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Main/Main.js';
 import Header from './Components/Header/Header.js';
-
+import Footer from './Components/Footer/Footer.js';
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Main />
+      <Footer></Footer>
     </div>
   );
 }
