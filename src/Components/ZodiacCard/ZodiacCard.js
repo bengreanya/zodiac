a6bb1fa74b50c4d91987cbe6d6abdf790192d80d
Ben Greanya <bengreanya@gmail.com>
2023-01-10 14:47:17 -0800

    got zodiac signs to render

---
+++ src/Components/ZodiacCard/ZodiacCard.js
@@ -0,0 +1,13 @@
+import './ZodiacCard.css';
+// import React from 'react';
+
+export default function ZodiacCard(props) {
+  return (
+    <div className="zodiacCard">
+      ZodiacCard
+      <img src={`images/${props.image}.png`} />
+      <h2>{props.name}</h2>
+      <p>{props.dates}</p>
+    </div>
+  );
+}
