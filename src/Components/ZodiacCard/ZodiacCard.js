417826b53927f8883df86ae4be15f145e3d62577
Ben Greanya <bengreanya@gmail.com>
2023-01-10 16:09:15 -0800

    added some styling

--- src/Components/ZodiacCard/ZodiacCard.js
+++ src/Components/ZodiacCard/ZodiacCard.js
@@ -4,7 +4,6 @@ import './ZodiacCard.css';
 export default function ZodiacCard(props) {
   return (
     <div className="zodiacCard">
-      ZodiacCard
       <img src={`images/${props.name}.png`} />
       <h2>{props.name}</h2>
       <p>{props.dates}</p>
